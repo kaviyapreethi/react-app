57a77fb1bc1c29d26e16f76460c5cdcd35b7ac60
kaviyapreethi <preethikaviya97@gmail.com>
2019-12-27 17:03:53 +0530

    Function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import ReactDOM from "react-dom";
 
 const element = <h1>Hello World</h1>;
 ReactDOM.render(element, document.getElementById("root"));
+
+function sayHello() {
+  for (var i = 0; i < 5; i++) {
+    console.log(i);
+    //let x = 1;
+  }
+  console.log(i); // can be accesible outside function
+  //console.log(x); // can not be accesible outside block
+  const y = 1;
+  //y = 2;  // cannot re-assign the value. It is a Syntax error : x is read-only
+  console.log(y);
+}
+sayHello();
+//So in ES6 we prefer ‘let’ to avoid access outside the block.
+//And prefer ‘const’ to avoid re-assigning the value.
